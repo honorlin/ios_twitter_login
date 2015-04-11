248a253861a9c986e4283a1f356c99eef23f639a
Honor Lin <tenhon.lin@gmail.com>
2015-04-12 02:31:26 +0800

    Initial Commit

---
+++ shanhuoy2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shanhuoy2
+//
+//  Created by Honor on 2015/4/12.
+//  Copyright (c) 2015年 Honor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
