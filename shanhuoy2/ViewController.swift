a1644e7b60ac8735f4423504322dbb507d47c4a5
Honor Lin <tenhon.lin@gmail.com>
2015-04-12 03:07:12 +0800

    first commit

--- shanhuoy2/ViewController.swift
+++ shanhuoy2/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import TwitterKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+        let logInButton = TWTRLogInButton(logInCompletion: {
+            (session: TWTRSession!, error: NSError!) in
+            // play with Twitter session
+            
+            
+        })
+        logInButton.center = self.view.center
+        self.view.addSubview(logInButton)
+
     }
 
     override func didReceiveMemoryWarning() {
